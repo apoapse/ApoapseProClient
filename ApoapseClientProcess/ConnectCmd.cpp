180bec917f47d7ecc17d07e1c3beccdd8269c8bc
Guillaume Puyal <contact@guillaume-puyal.com>
2017-07-05 07:51:04 +0200

    Added test classes for the command system

---
+++ ApoapseClientProcess/ConnectCmd.cpp
@@ -0,0 +1,45 @@
+#include "stdafx.h"
+#include "Command.h"
+#include "Common.h"
+#include "CommandsManager.h"
+
+class ConnectCmd : public Command
+{
+	CommandInfo& GetInfo() const override
+	{
+		static auto info = CommandInfo();
+		info.command = Commands::CONNECT;
+		info.fields =
+		{
+			CommandField{ "user", FieldRequirement::ANY_MENDATORY, FIELD_VALUE_VALIDATOR(std::string, ConnectCmd::Test) },
+			CommandField{ "pass", FieldRequirement::ANY_MENDATORY, FIELD_VALUE(std::string) },
+			CommandField{"test_array", FieldRequirement::ANY_OPTIONAL, FIELD_ARRAY_VALIDATOR(int, ConnectCmd::TestArray)}
+		};
+
+		return info;
+	}
+
+	void Main()
+	{
+		std::function<bool(std::string)> test = ConnectCmd::Test;
+	}
+
+	static bool Test(const std::string& str)
+	{
+		
+
+		return true;
+	}
+
+	static bool TestArray(const std::vector<int>& vec)
+	{
+		for (auto item : vec)
+		{
+			LOG << item;
+		}
+
+		return false;
+	}
+};
+
+APOAPSE_COMMAND_REGISTER(ConnectCmd, Commands::CONNECT);
--- ApoapseClientProcess/main.cpp
+++ ApoapseClientProcess/main.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "Common.h"
 #include "GenericConnection.h"//TEMP
+#include "CommandsManager.h"//temp
+#include "NetworkPayload.h"
 
 #ifdef UNIT_TESTS
 #include "UnitTestsManager.h"
@@ -27,12 +29,46 @@ int main(int argcount, char* argv[])
 #endif //UNIT_TESTS
 
 	{
+		MessagePackSerializer serializer;
+		std::vector<int> arrayData{ 6, 7, 8, -625 };
+
+		serializer.Group("",
+		{
+			MSGPK_ORDERED_APPEND(serializer, "user", "guillaume"),
+			MSGPK_ORDERED_APPEND(serializer, "pass", "ddqsdqzdssfzsedqsd5sdf8se9sf5"),
+			MSGPK_ORDERED_APPEND_ARRAY(serializer, int, "test_array", arrayData),
+
+			//[&] { serializer.OrderedAppendArray<int>("test_array", std::vector<int> { 1, 2, 3 }); }
+		});
+
+		auto payloadBody = serializer.GetMessagePackBytes();
+		auto header = NetworkPayload::GenerateHeader(Commands::CONNECT, payloadBody);
+		std::vector<byte> data(header.begin(), header.end());
+		data.insert(data.end(), payloadBody.begin(), payloadBody.end());
+
+		auto payload = std::make_shared<NetworkPayload>();
+		auto cmd = CommandsManager::GetInstance().CreateCommand(Commands::CONNECT);
+
+		{
+			std::array<byte, READ_BUFFER_SIZE> arr;
+			std::copy(data.begin(), data.end(), arr.begin());
+
+			Range<std::array<byte, READ_BUFFER_SIZE>> range(arr, data.size());
+			payload->Insert(range);
+
+			ASSERT(payload->headerInfo->command == Commands::CONNECT);
+		}
+		
+		cmd->Parse(payload);
+	}
+
+	{
 		boost::asio::io_service ioService;
 		auto connection = std::make_shared<GenericConnection>(ioService);
 		connection->Connect("127.0.0.1", 5700);
 
 		auto test = std::vector<byte> { 'a', 'b', 'c', 'd' };
-		connection->Send(IdsCommand::CONNECT, std::make_unique<std::vector<byte>>(test), nullptr);
+		connection->Send(Commands::CONNECT, std::make_unique<std::vector<byte>>(test), nullptr);
 		ioService.run();
 	}
 
