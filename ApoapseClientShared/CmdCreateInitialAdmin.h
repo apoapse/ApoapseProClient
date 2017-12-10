d6caf06e252813858594bca50201e29ad82fba7e
Guillaume Puyal <contact@guillaume-puyal.com>
2017-12-10 15:56:16 +0100

    Completed CreateInitialAdmin cmd

--- ApoapseClientShared/CmdCreateInitialAdmin.h
+++ ApoapseClientShared/CmdCreateInitialAdmin.h
@@ -1,6 +1,32 @@
 #pragma once
 #include "Command.h"
+#include "IUsersManager.h"
+#include "Username.h"
+#include "CryptographyTypes.hpp"
 class ApoapseClient;
+
+class DummyUsersManager : public IUsersManager
+{
+	const Username& m_uniqueUser;
+	const PublicKeyBytes& m_userIdKey;
+
+public:
+	DummyUsersManager(const Username& uniqueUser, const PublicKeyBytes& idPublicKey)
+		: m_uniqueUser(uniqueUser)
+		, m_userIdKey(idPublicKey)
+	{
+	}
+
+	bool DoesUserExist(const Username& username) const override
+	{
+		return (username == m_uniqueUser);
+	}
+
+	PublicKeyBytes GetUserIdentityPublicKey(const Username& username) const override
+	{
+		return m_userIdKey;
+	}
+};
 
 class CmdCreateInitialAdmin : public Command
 {
